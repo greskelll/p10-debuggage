ae330c0014aa264bb9400d707ccc7ca2dfb62cf7
greskelll <jonattan44@hotmail.fr>
2023-05-02 10:23:44 +0200

    debug slider sans message err console

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,22 +12,22 @@ const Slider = () => {
 		ex: 2021-05-12 > 2022-11-25 > 2023-03-13  */
 		new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
 	);
-	const nextCard = () => {
+	/* pb byDateDesc undefined au premier changement de state */
+	const nextCard = (length) => {
 		setTimeout(
 			/* il faut comparer l'index a la longueur du tableau d'image -1 puisque l'index commence à zéro */
-			() => setIndex(index === byDateDesc.length - 1 ? 0 : index + 1),
+			() => setIndex(index === length - 1 ? 0 : index + 1),
 			5000
 		);
 	};
 	useEffect(() => {
-		nextCard();
+		nextCard(byDateDesc?.length);
 	});
 	return (
 		<div className="SlideCardList">
 			{byDateDesc?.map((event, idx) => (
-				<>
+				<div key={`pourquoi${event.title + event.id}`}>
 					<div
-						key={event.title + event.id}
 						className={`SlideCard SlideCard--${
 							index === idx ? 'display' : 'hide'
 						}`}
@@ -43,19 +43,20 @@ const Slider = () => {
 					</div>
 					<div className="SlideCard__paginationContainer">
 						<div className="SlideCard__pagination">
-							{byDateDesc.map((_, radioIdx) => (
+							{byDateDesc?.map((_, radioIdx) => (
 								<input
+									key={`radio${event.id}`}
 									/* changement key pour corriger key non unique ? p-e */
-									key={event.id}
 									type="radio"
 									name="radio-button"
 									/* changement comparaison index === radioIdx plutot que idx === radioIdx */
 									checked={index === radioIdx}
+									readOnly /* pas de nature à naviguer */
 								/>
 							))}
 						</div>
 					</div>
-				</>
+				</div>
 			))}
 		</div>
 	);
